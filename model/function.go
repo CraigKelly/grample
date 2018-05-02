6fe87e8650b4ab94d2b8adfa01df923598adccd2
221019831 <Craig.Kelly@ge.com>
2018-05-02 20:22:24 +0000

    function skel with test

--- model/function.go
+++ model/function.go
@@ -1,11 +1,38 @@
 package model
 
 import (
+	"math"
+
 	"github.com/pkg/errors"
 )
 
-// Function represents a function of Variables or a CPT.
-// TODO: describe order of Table
+// A Function represents a function of Variables (which may be a CPT or a more
+// general factor). In a Markov network, this is defined on a clique. Note that
+// factors in a Markox network are assumed to return NON-normalized
+// probabilities.  You need Z (the partition function) to normalize to "real"
+// probabilities.
+//
+// The actual ordering of the Table values matches the order of the variables,
+// where the variables are ordered from "most" to "least" significant. (This is
+// the same order used in UAI data files). As a example, let's assume 3 boolean
+// variables in the order [A, B, C]. Let's further assume that this join
+// probability distribution is completely uniform: The CPT would look like:
+//
+//   ABC  P(A,B,C)
+//   ---  --------
+//   000   0.125
+//   001   0.125
+//   010   0.125
+//   011   0.125
+//   100   0.125
+//   101   0.125
+//   110   0.125
+//   111   0.125
+//
+// And our Table array would be in the same order. Since we assume that a
+// variable's domain is [0, C-1] where C is cardinality (e.g. a boolean var has
+// C=2 with values {0,1}), we can map directory from an ordered list of values
+// (in the same order as the variables) to an index in the table (see Eval).
 type Function struct {
 	Name  string      // Name for function (or just a 0-based index in UAI formats)
 	Vars  []*Variable // Vars in function
@@ -13,7 +40,7 @@ type Function struct {
 }
 
 // Check returns an error if any problem is found
-func (f Function) Check() error {
+func (f *Function) Check() error {
 	expTableSize := 0
 
 	if len(f.Vars) > 0 {
@@ -31,7 +58,44 @@ func (f Function) Check() error {
 		return errors.Errorf("Function %s expected table size %d, found %d", f.Name, expTableSize, len(f.Table))
 	}
 
-	// TODO: ensure Table is valid (sums to 1.0 for in all cases)?
-
 	return nil
+}
+
+// Eval returns the result of the function
+func (f *Function) Eval(values []int) (float64, error) {
+	i, err := f.calcIndex(values)
+	if err != nil {
+		return math.NaN(), err
+	}
+
+	if i < 0 || i >= len(f.Table) {
+		return math.NaN(), errors.Errorf("Could not find table entry for values %v", values)
+	}
+
+	return f.Table[i], nil
+}
+
+// calcIndex generates an index into the table given a vector of values.
+func (f *Function) calcIndex(values []int) (int, error) {
+	if len(values) != len(f.Vars) {
+		return -1, errors.Errorf("Value vector %v does not match variables", values)
+	}
+
+	// Work from least significant to most significant. (This is not optional:
+	// each "digit" can be a different size).
+	digit := 1
+	location := 0
+
+	for i := len(values) - 1; i >= 0; i-- {
+		val := values[i]
+		card := f.Vars[i].Card
+		if val < 0 || val >= card {
+			return -1, errors.Errorf("Value %d invalid for cardinality %d for var %s", val, card, f.Vars[i].Name)
+		}
+
+		location += digit * val
+		digit *= card
+	}
+
+	return location, nil
 }
---
+++ model/function_test.go
@@ -0,0 +1,137 @@
+package model
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Make sure that Check actually catches problems
+func TestFuncBadCheck(t *testing.T) {
+	assert := assert.New(t)
+
+	// handy short vars for below
+	v0 := &Variable{"V0", 0, []float64{}}
+	v1 := &Variable{"V1", 1, []float64{1.0}}
+	v2 := &Variable{"V2", 2, []float64{0.25, 0.75}}
+	v3 := &Variable{"V2", 3, []float64{0.25, 0.70, 0.05}}
+
+	// quick sanity check
+	assert.NoError(v0.Check())
+	assert.NoError(v1.Check())
+	assert.NoError(v2.Check())
+	assert.NoError(v3.Check())
+
+	cases := []*Function{
+		{"Bad-NoVarHaveTable", []*Variable{}, []float64{0.5, 0.5}},
+		{"Bad-0Var", []*Variable{v0}, []float64{}},
+
+		{"Bad-1Var1BadTable", []*Variable{v1}, []float64{0.5, 0.5}},
+		{"Bad-1Var2BadTable", []*Variable{v2}, []float64{0.5, 0.5, 0.5}},
+		{"Bad-1Var3BadTable", []*Variable{v3}, []float64{0.5}},
+
+		{"Bad-2VarBadVar", []*Variable{v2, v0}, []float64{0.5, 0.5}},
+		{"Bad-2VarBadTableHi", []*Variable{v2, v2}, []float64{0.5, 0.5, 0.5, 0.5, 0.5}},
+		{"Bad-2VarBadTableLo", []*Variable{v2, v2}, []float64{0.5, 0.5}},
+
+		{"Bad-3VarBadTable", []*Variable{v1, v2, v3}, []float64{0.5, 0.5, 0.5, 0.5, 0.5}},
+	}
+
+	for _, f := range cases {
+		assert.Error(f.Check())
+	}
+}
+
+// Make sure we're OK with valid functions
+func TestFuncGoodCheck(t *testing.T) {
+	assert := assert.New(t)
+
+	// handy short vars for below
+	v1 := &Variable{"V1", 1, []float64{1.0}}
+	v2 := &Variable{"V2", 2, []float64{0.25, 0.75}}
+	v3 := &Variable{"V2", 3, []float64{0.25, 0.70, 0.05}}
+
+	// quick sanity check
+	assert.NoError(v1.Check())
+	assert.NoError(v2.Check())
+	assert.NoError(v3.Check())
+
+	cases := []*Function{
+		{"Good-NoVar", []*Variable{}, []float64{}},
+
+		{"Good-1Var1", []*Variable{v1}, []float64{0.5}},
+		{"Good-1Var2", []*Variable{v2}, []float64{0.5, 0.5}},
+		{"Good-1Var3", []*Variable{v3}, []float64{0.5, 0.5, 0.5}},
+
+		{"Good-2VarBin", []*Variable{v2, v2}, []float64{0.5, 0.5, 0.5, 0.5}},
+		{"Good-2VarMad", []*Variable{v2, v3}, []float64{0.5, 0.5, 0.5, 0.5, 0.5, 0.5}},
+
+		{"Good-3VarAll", []*Variable{v1, v2, v3}, []float64{0.5, 0.5, 0.5, 0.5, 0.5, 0.5}},
+		{"Good-3VarNo1", []*Variable{v3, v2, v2}, []float64{0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}},
+	}
+
+	for _, f := range cases {
+		assert.NoError(f.Check())
+	}
+}
+
+// Make sure we correctly handle bad eval
+func TestFuncTestEval(t *testing.T) {
+	assert := assert.New(t)
+
+	// handy short vars for below
+	v2 := &Variable{"V2", 2, []float64{0.25, 0.75}}
+	v3 := &Variable{"V2", 3, []float64{0.25, 0.70, 0.05}}
+
+	// quick sanity check
+	assert.NoError(v2.Check())
+	assert.NoError(v3.Check())
+
+	f := &Function{
+		"TestTable",
+		[]*Variable{v2, v3},
+		[]float64{
+			0.01, // 0 0
+			1.02, // 0 1
+			2.03, // 0 2
+			3.04, // 1 0
+			4.05, // 1 1
+			5.06, // 1 2
+		},
+	}
+
+	passCases := []struct {
+		values   []int
+		expected float64
+	}{
+		{[]int{0, 0}, 0.01},
+		{[]int{0, 1}, 1.02},
+		{[]int{0, 2}, 2.03},
+		{[]int{1, 0}, 3.04},
+		{[]int{1, 1}, 4.05},
+		{[]int{1, 2}, 5.06},
+	}
+
+	failCases := [][]int{
+		{},
+		{0},
+		{0, 0, 0},
+		{2, 0},
+		{0, 3},
+	}
+
+	const EPS float64 = 1e-14
+
+	for _, c := range passCases {
+		v, e := f.Eval(c.values)
+		assert.NoError(e)
+		assert.InEpsilon(c.expected, v, EPS)
+	}
+
+	for _, c := range failCases {
+		v, e := f.Eval(c)
+		assert.Error(e)
+		assert.True(math.IsNaN(v))
+	}
+}
