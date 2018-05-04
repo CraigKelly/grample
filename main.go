5a2c49b4f4b400b5550bf5090b1fcc80b5484729
Craig Kelly <craig.n.kelly@gmail.com>
2018-05-04 18:43:20 -0500

    todo comment update

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "github.com/CraigKelly/grample/cmd"
 
 // TODO: simple no evidence Gibbs sampling
-// TODO: need some benchmarks
+// TODO: checkpointing for chains (so we can freeze and continue) - which means
+//       model/sampler/chain all need to be included?
+// TODO: need some benchmarks - and make sure to output
 
 func main() {
 	cmd.Execute()
