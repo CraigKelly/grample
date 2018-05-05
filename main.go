bd1ad04498c9ef7abf3e69705a730d97f51e1397
Craig Kelly <craig.n.kelly@gmail.com>
2018-05-05 13:12:55 -0500

    todo comment tweak

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import "github.com/CraigKelly/grample/cmd"
 // TODO: checkpointing for chains (so we can freeze and continue) - which means
 //       model/sampler/chain all need to be included?
 // TODO: need some benchmarks - and make sure to output
+// TODO: accept a starting seed, which is used to create a master rand source
 
 func main() {
 	cmd.Execute()
