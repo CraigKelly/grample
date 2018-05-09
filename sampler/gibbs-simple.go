b95eb0d5419f9aa3f25c15084c881202127b6257
221019831 <Craig.Kelly@ge.com>
2018-05-09 16:14:46 +0000

    Gibbs simple work - just need factor eval now

--- sampler/gibbs-simple.go
+++ sampler/gibbs-simple.go
@@ -1,11 +1,11 @@
 package sampler
 
 import (
+	"math"
 	"math/rand"
 
 	"github.com/CraigKelly/grample/model"
 	"github.com/pkg/errors"
-	"gonum.org/v1/gonum/stat/sampleuv"
 )
 
 // GibbsSimple is our baseline, simple to code Gibbs sampler
@@ -13,7 +13,7 @@ type GibbsSimple struct {
 	src         rand.Source
 	rnd         *rand.Rand
 	pgm         *model.Model
-	varSelector sampleuv.Sampler
+	varSelector VarSampler
 	last        []float64
 }
 
@@ -23,19 +23,37 @@ func NewGibbsSimple(src rand.Source, m *model.Model) (*GibbsSimple, error) {
 		return nil, errors.New("No model supplied")
 	}
 
+	// Use log space for factors
+	for _, f := range m.Funcs {
+		err := f.UseLogSpace()
+		if err != nil {
+			return nil, errors.Wrapf(err, "Could not convert function %v to Log Space", f.Name)
+		}
+	}
+
+	uniform, err := NewUniformSampler(src)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create uniform sampler in Gibbs Simple sample")
+	}
+
 	s := &GibbsSimple{
-		src:  src,
-		rnd:  rand.New(src),
-		pgm:  m,
-		last: make([]float64, len(m.Vars)),
+		src:         src,
+		rnd:         rand.New(src),
+		pgm:         m,
+		varSelector: uniform,
+		last:        make([]float64, len(m.Vars)),
 	}
 
 	// Starting point in the sample space - note that the next call to Sample
 	// will return the next sample, and not this one so our user will never
 	// see this starting point unless they explicitly look for it.
 	for i, v := range s.pgm.Vars {
+		val, err := uniform.ValSample(v.Card)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Could not generate start sample for variable %v", v.Name)
+		}
 		// Select value for every variable with uniform prob
-		s.last[i] = float64(s.rnd.Int31n(int32(v.Card)))
+		s.last[i] = float64(val)
 	}
 
 	return s, nil
@@ -47,11 +65,46 @@ func (g *GibbsSimple) Sample(s []float64) error {
 		return errors.Errorf("Sample size %d != Var size %d in model %s", len(s), len(g.pgm.Vars), g.pgm.Name)
 	}
 
-	varIdx := 0    // TODO: select variable with g.varSelector
-	nextVal := 0.0 // TODO: Create our next sample with g.varSamplers[varIdx]
+	// Select next variable to sample
+	varIdx, err := g.varSelector.VarSample(g.pgm.Vars)
+	if err != nil {
+		return errors.Wrapf(err, "Could not sample from vars in model %s", g.pgm.Name)
+	}
+	sampleVar := g.pgm.Vars[varIdx]
+
+	// Find all related factors and marginalize for sampleVar
+	sampleWeights := make([]float64, sampleVar.Card)
+
+	// TODO: Find functions for sampleVar
+	// TODO: Call each function with all vals for sampleVar and ADD all vals to sampleWeights
+
+	// Convert sampleWeights from log space - and gather a total while we're at it
+	totWeights := 0.0
+	for i, w := range sampleWeights {
+		v := math.Exp(w)
+		totWeights += v
+		sampleWeights[i] = v
+	}
+
+	// Select value based on the factor weights for our current variable
+	r := g.rnd.Float64() * totWeights
+	nextVal := -1
+	for i, w := range sampleWeights {
+		if r <= w {
+			// Remember, it's an array of weights from 0 -> Card-1: we are
+			// selecting an index based on those weights
+			nextVal = i
+			break
+		}
+		r -= w
+	}
+
+	if nextVal < 0 {
+		return errors.Errorf("Failed to select a value from var %v, Exp(factor-weights)==%v", sampleVar.Name, sampleWeights)
+	}
 
 	// Update saved copy with new value and copy to caller's sample
-	g.last[varIdx] = nextVal
+	g.last[varIdx] = float64(nextVal)
 	copy(s, g.last)
 
 	return nil
--- sampler/sampler.go
+++ sampler/sampler.go
@@ -1,9 +1,61 @@
 package sampler
 
-// A Sampler samplers from the given model. Note that we follow gonum
-// conventions and sample in place (although we return an error). Although
-// it's not explicit, it is assumed that the sample is the same size as a
-// variable list
-type Sampler interface {
-	Sample(s []float64) error
+import (
+	"math/rand"
+
+	"github.com/CraigKelly/grample/model"
+	"github.com/pkg/errors"
+)
+
+// A ValSampler returns a sample given a cardinality. We assume the possible
+// values are 0 to Cardinality-1.
+type ValSampler interface {
+	ValSample(card int) (int, error)
+}
+
+// A VarSampler selects from an array of variables with some probability
+type VarSampler interface {
+	VarSample(vs []*model.Variable) (int, error)
+}
+
+// UniformSampler provides uniform sampling for our interfaces
+type UniformSampler struct {
+	src rand.Source
+	rnd *rand.Rand
+}
+
+// NewUniformSampler creates a new uniform sampler
+func NewUniformSampler(src rand.Source) (*UniformSampler, error) {
+	s := &UniformSampler{
+		src: src,
+		rnd: rand.New(src),
+	}
+	return s, nil
+}
+
+// ValSample implements ValSampler interface
+func (s *UniformSampler) ValSample(card int) (int, error) {
+	if card < 1 {
+		return -1, errors.New("Can not sample if Cardinality < 1")
+	}
+	if card == 1 {
+		return -1, nil
+	}
+
+	return int(s.rnd.Int31n(int32(card))), nil
+}
+
+// VarSample implements VarSample interface
+func (s *UniformSampler) VarSample(vs []*model.Variable) (int, error) {
+	vsLen := len(vs)
+	if vsLen < 1 {
+		return 0, errors.New("Can not sample from an empty variable list")
+	}
+
+	i, e := s.ValSample(vsLen)
+	if e != nil {
+		return -1, e
+	}
+
+	return i, nil
 }
