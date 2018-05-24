a4bb84a92621a710d4df3d266018572d80927bbf
221019831 <Craig.Kelly@ge.com>
2018-05-24 18:19:23 +0000

    Increase error calc test coverage

--- model/uai_test.go
+++ model/uai_test.go
@@ -134,17 +134,27 @@ func TestUAIMarSolFile(t *testing.T) {
 
 	// Handy to know: our simple one.uai model has a single factor of 0.25/0.75
 	// and models default the vars to have uniform marginals. So we know the
-	// starting TAE should be 0.5
-	score, _, err := s.AbsError(m)
+	// starting Total AE should be 0.5 and Max AE should be 0.25
+	totAE, maxAE, err := s.AbsError(m)
 	assert.NoError(err)
-	assert.InEpsilon(0.5, score, 1e-8)
+	assert.InEpsilon(0.50, totAE, 1e-8)
+	assert.InEpsilon(0.25, maxAE, 1e-8)
+
+	// Hellinger we just calculate directly
+	p1 := math.Pow(math.Sqrt(0.75)-math.Sqrt(0.50), 2)
+	p2 := math.Pow(math.Sqrt(0.25)-math.Sqrt(0.50), 2)
+	hellExp := (p1 + p2) / math.Sqrt2
+	hell, err := s.HellingerError(m)
+	assert.NoError(err)
+	assert.InEpsilon(hellExp, hell, 1e-8)
 
 	// Also check non-normed model vars
 	m.Vars[0].Marginal[0] = 250.0
 	m.Vars[0].Marginal[1] = 250.0
-	score, _, err = s.AbsError(m)
+	totAE, maxAE, err = s.AbsError(m)
 	assert.NoError(err)
-	assert.InEpsilon(0.5, score, 1e-8)
+	assert.InEpsilon(0.50, totAE, 1e-8)
+	assert.InEpsilon(0.25, maxAE, 1e-8)
 }
 
 // Test reading evidence
