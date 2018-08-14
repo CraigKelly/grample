4c40401db5c94399f58c912e1f966592faac5d13
221019831 <Craig.Kelly@ge.com>
2018-08-14 21:25:18 +0000

    backlog update

--- main.go
+++ main.go
@@ -6,16 +6,7 @@ import "github.com/CraigKelly/grample/cmd"
 // TODO: need a collapsing Gibbs sample: both static(at begin) or dynamic(during chain)
 // TODO: combine adaptive/collapse options
 
-// TODO: trace file should include final scores for easy read back once we get started
-
 // TODO: at least one unit test for cmd package - and maybe a benchmark?
-
-// TODO: at least one unit test and one benchmark for rand
-
-// TODO: checkpointing for chains (so we can freeze and continue) - which means
-//       model/sampler/chain all need to be included?
-
-// TODO: web frontend for monitor so we can track what's going on?
 
 func main() {
 	cmd.Execute()
