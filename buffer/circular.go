210cc0248a860999f8f9b870ac97046d7acfc480
221019831 <Craig.Kelly@ge.com>
2018-08-15 21:32:08 +0000

    track total seen in circular buffer

--- buffer/circular.go
+++ buffer/circular.go
@@ -4,10 +4,11 @@ package buffer
 // the first and second halves of the integers collected in the order that they
 // were appended.
 type CircularInt struct {
-	buffer  []int // actual storage
-	pos     int   // Current position in buffer
-	BufSize int   // BufSize is the fixed number of ints maintained in memory
-	Count   int   // Count is the number of ints in memory. Will always be <= BufSize
+	buffer    []int // actual storage
+	pos       int   // Current position in buffer
+	BufSize   int   // BufSize is the fixed number of ints maintained in memory
+	Count     int   // Count is the number of ints in memory. Will always be <= BufSize
+	TotalSeen int64 // TotalSeen is the total number of times Add has been called
 }
 
 // NewCircularInt creates a new circular buffer of totalSize. If totalSize is
@@ -32,6 +33,8 @@ func (c *CircularInt) nextPos() int {
 
 // Add appends the given int to the buffer, overwriting the oldest entry
 func (c *CircularInt) Add(i int) error {
+	c.TotalSeen++
+
 	c.buffer[c.pos] = i
 
 	c.pos = c.nextPos()
